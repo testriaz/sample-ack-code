e92c42dc65cadb3f169faccdaf90fbda471537cd
testriaz <riaz.engg@gmail.com>
2023-07-20 10:07:11 +0530

    Test pkg repo.

---
+++ src/main/java/TestRepoPackage.java
@@ -0,0 +1,11 @@
+import com.test.pkg.implement.Test;
+
+public class TestRepoPackage {
+
+    public static void main(String arg[]){
+
+
+        Test t = new Test();
+        t.display("355");
+    }
+        }
